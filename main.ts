b5e4c3f88c3292dd0520d68d3f33503f855e5b42
iwearthecrowns <iwearthecrowns@users.noreply.github.com>
2020-06-08 00:26:00 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,25 @@
+let mySprite = sprites.create(img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, SpriteKind.Player)
+let number = "1"
+let number2 = number
+number = "2"
+mySprite.say(number2, 5000)
+number2 = number
+mySprite.say(number2, 5000)
+mySprite.say("This is the variable number=" + number + (" AND...This is the variable number2=" + number2))
